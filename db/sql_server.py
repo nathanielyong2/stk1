62ed6cbfe956f073f6be406df6024f2e641c7565
Nathaniel Yong <nathanielyongwork1@gmail.com>
2021-08-03 14:13:04 -0400

    commit

--- db/sql_server.py
+++ db/sql_server.py
@@ -1,5 +1,8 @@
+import os
 import csv
 import pyodbc
+
+rows = 0
 
 conn = pyodbc.connect('Driver={SQL Server};'
                       'Server=LAPTOP-KHUTVNS5;'
@@ -7,13 +10,28 @@ conn = pyodbc.connect('Driver={SQL Server};'
                       'Trusted_Connection=yes;')
 cursor = conn.cursor()
 
-with open('dummy.txt', mode='r') as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=',')
-    for row in csv_reader:
-        cursor.execute(
-            f"INSERT INTO stocks.dbo.stk_price_hist VALUES('{row[0]}', '{row[1]}', '{row[2]}', {row[3]}, {row[4]}, {row[5]}, {row[6]}, {row[7]})")
+basedir = os.path.abspath(os.path.dirname(__file__))
+path = os.path.abspath(os.path.join(basedir, '../..', 'NASDAQ_2021'))
+
+for filename in os.listdir(path):
+    file = os.path.join(path, filename)
+
+    if os.path.isfile(file):
+        stk_filename = os.path.splitext(filename)[0]
+        exch = stk_filename.split('_')[0]
+
+        with open(f'{file}', mode='r') as csv_file:
+            csv_reader = csv.reader(csv_file, delimiter=',')
+            firstLine = True
+
+            for row in csv_reader:
+                if firstLine:
+                    firstLine = False
+                else:
+                    cursor.execute(
+                        f"INSERT INTO stocks.dbo.stk_price_hist VALUES('{exch}', '{row[0]}', '{row[1]}', {row[2]}, {row[3]}, {row[4]}, {row[5]}, {row[6]})")
+                    rows += 1
+
 cursor.commit()
 
-cursor.execute('SELECT * FROM stocks.dbo.stk_price_hist')
-for row in cursor:
-    print(row)
+print(f'Finished inserting {rows} rows')
