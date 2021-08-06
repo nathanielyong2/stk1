dfe4b801931098cac94c54afc58e8bd9422e6b81
Nathaniel Yong <nathanielyongwork1@gmail.com>
2021-08-06 18:17:11 -0400

    load data

--- db/load_data_EXCH.sql
+++ db/load_data_EXCH.sql
@@ -1,6 +1,6 @@
 USE stocks
 
-INSERT INTO exch VALUES ('NASDAQ', 'NASDAQ Stock Exchange', 'USD')
-INSERT INTO exch VALUES ('NYSE', 'New York Stock Exchange', 'USD')
-INSERT INTO exch VALUES ('TSX', 'Toronto Stock Exchange', 'CAD')
-INSERT INTO exch VALUES ('AMEX', 'American Stock Exchange', 'USD')
+INSERT INTO exch VALUES ('NASDAQ', 'NASDAQ Stock Exchange', 'USD');
+INSERT INTO exch VALUES ('NYSE', 'New York Stock Exchange', 'USD');
+INSERT INTO exch VALUES ('TSX', 'Toronto Stock Exchange', 'CAD');
+INSERT INTO exch VALUES ('AMEX', 'American Stock Exchange', 'USD');
