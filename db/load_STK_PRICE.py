d9f87f2b948e90d73ee98914d3b9004badd909c0
Nathaniel Yong <nathanielyongwork1@gmail.com>
2021-08-12 22:41:09 -0400

    improved load_stk_price.py

--- db/load_STK_PRICE.py
+++ db/load_STK_PRICE.py
@@ -4,12 +4,12 @@ import pyodbc
 import socket
 
 rows = 0
-current_file_linenum =0
+current_file_linenum = 0
 
-server_name='Server=' + socket.gethostname() + ';'
-connect_string='Driver={SQL Server};' + server_name + 'Database=stocks;' + 'Trusted_Connection=yes;'
-conn = pyodbc.connect(connect_string)
-cursor = conn.cursor()
+server_name = 'Server=' + socket.gethostname() + ';'
+connect_string = 'Driver={SQL Server};' + server_name + 'Database=stocks;' + 'Trusted_Connection=yes;'
+connection = pyodbc.connect(connect_string)
+cursor = connection.cursor()
 print('Connection set...')
 
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -24,24 +24,28 @@ for filename in os.listdir(path):
         exch = stk_filename.split('_')[0]
 
         with open(f'{file}', mode='r') as csv_file:
-            current_file_linenum=0
             csv_reader = csv.reader(csv_file, delimiter=',')
-            firstLine = True
+            next(csv_reader)
             
-            for row in csv_reader:
-                current_file_linenum+=1
-                if firstLine:
-                    firstLine = False
-                else:
-                    # print(f'Current line : {current_file_linenum}')
-                    cursor.execute(
-                        f"INSERT INTO stocks.dbo.stk_price VALUES('{exch}', '{row[0]}', '{row[1]}', {row[2]}, {row[3]}, {row[4]}, {row[5]}, {row[6]})")
-                    rows += 1
+            current_file_linenum = 0
+            stk_values = []
 
-        cursor.commit()
+            for row in csv_reader:
+                current_file_linenum += 1
+                # print(f'Current line : {current_file_linenum}')
+                
+                stk_info = (row[0], row[1], row[2], row[3], row[4], row[5], row[6])
+                stk_values.append(stk_info)
+                
+            cursor.executemany(
+                        f"INSERT INTO stocks.dbo.stk_price VALUES('{exch}', ?, ?, ?, ?, ?, ?, ?)", stk_values)
+            connection.commit()
+            
         print(f'End : Loaded {current_file_linenum} rows for file {filename}')
+        rows += current_file_linenum
     else:
         print(f'Error! : Invalid file : {filename}')
 
 print(f'==> Total rows loaded = {rows}')
 
+connection.close()
